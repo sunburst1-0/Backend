d7d6521ec0522bb1cae4e1d2d8c01924aa424418
ISURU <isurudr40@gmail.com>
2020-03-29 22:03:31 +0530

    added

--- src/main/java/com/data/processor/BasicController.java
+++ src/main/java/com/data/processor/BasicController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import com.data.repositories.ApplianceTypeRepository;
 import com.data.repositories.RoomSizeRepository;
 import com.data.service.RoomSizeService;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping(value = "/kafka")
 public class BasicController {
