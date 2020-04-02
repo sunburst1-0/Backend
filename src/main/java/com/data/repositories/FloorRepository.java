fb2ca2951cb5b6d9bdd960791bfedec043598e11
ISURU <isurudr40@gmail.com>
2020-04-02 21:13:20 +0530

    added

---
+++ src/main/java/com/data/repositories/FloorRepository.java
@@ -0,0 +1,23 @@
+package com.data.repositories;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import org.springframework.stereotype.Repository;
+
+import com.data.model.Floor;
+import com.data.model.IRsignal;
+import com.data.model.Location;
+import com.data.model.RoomSize;
+import com.data.model.SensorType;
+
+@Repository
+public interface FloorRepository extends JpaRepository<Floor, Integer> {
+	
+	
+	
+
+}
