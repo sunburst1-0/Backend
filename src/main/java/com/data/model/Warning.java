cc811d98f7f457a5b3c4934ba7840319b4243258
ISURU <isurudr40@gmail.com>
2020-05-02 23:21:02 +0530

    warning generator

---
+++ src/main/java/com/data/model/Warning.java
@@ -0,0 +1,80 @@
+package com.data.model;
+
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * Warning  entity.
+ */
+@Entity
+@Table(name = "warning")
+public class Warning {
+	
+	
+	@Id
+	@Column(name="id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+	
+	
+	@Column(name="priority")
+	private String priority;
+	
+	@Column(name="status")
+	private String status;
+	
+		
+	@Column(name="warn_message")
+	private String warnMessage;
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getPriority() {
+		return priority;
+	}
+
+
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+
+
+	public String getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+
+	public String getWarnMessage() {
+		return warnMessage;
+	}
+
+
+	public void setWarnMessage(String warnMessage) {
+		this.warnMessage = warnMessage;
+	}
+	
+	
+
+}
