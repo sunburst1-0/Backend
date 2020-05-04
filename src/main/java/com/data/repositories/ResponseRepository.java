4f2c575491d9a099cdd2d5a7271e3b06c09031d1
ISURU <isurudr40@gmail.com>
2020-05-04 23:48:29 +0530

    added

---
+++ src/main/java/com/data/repositories/ResponseRepository.java
@@ -0,0 +1,12 @@
+package com.data.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.data.model.Response;
+
+
+
+@Repository
+public interface ResponseRepository extends JpaRepository<Response, Integer> {
+
+}
